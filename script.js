009a91fc04dc68722af393f83ae7d2aa38682c4c
Rafaella Duarte <rafaella.d.d.carvalho@gmail.com>
2024-12-16 21:38:21 -0300

    add recipe in web site

--- script.js
+++ script.js
@@ -3,22 +3,18 @@ let guarnicoes = [];
 let saladas = [];
 let pratosRapidos = [];
 
-// Função para carregar os JSONs
 async function carregarJSONs() {
     try {
-        // Carregando os dados dos arquivos JSON da pasta 'pratos'
         const pratosResponse = await fetch('pratos/pratosPrincipais.json');
         const rapidosResponse = await fetch('pratos/pratosRapidos.json');
         const guarnicoesResponse = await fetch('pratos/guarnicoes.json');
         const saladasResponse = await fetch('pratos/saladas.json');
         
-        // Convertendo os JSONs para objetos JavaScript
         const rapidosData = await rapidosResponse.json();
         const pratosData = await pratosResponse.json();
         const guarnicoesData = await guarnicoesResponse.json();
         const saladasData = await saladasResponse.json();
 
-        // Atribuindo os dados aos arrays
         pratosRapidos = rapidosData.pratos_rapidos || [];
         pratosPrincipais = pratosData.pratos_principais || [];
         guarnicoes = guarnicoesData.guarnicoes || [];
@@ -29,14 +25,13 @@ async function carregarJSONs() {
     }
 }
 
-// Função para gerar cardápio aleatório
 async function gerarCardapio() {
-    // Verifique se os JSONs foram carregados corretamente antes de tentar acessar os dados
     if (!pratosPrincipais || !guarnicoes || !saladas || !pratosRapidos) {
         await carregarJSONs();
     }
 
-    // Certifique-se de que os arrays estão corretamente carregados
+    hideReceitas()
+
     if (
         !Array.isArray(pratosPrincipais) || !Array.isArray(guarnicoes) ||
         !Array.isArray(saladas) || !Array.isArray(pratosRapidos)
@@ -45,35 +40,105 @@ async function gerarCardapio() {
         return;
     }
 
-
-    // Obtendo o tipo de refeição selecionado
     const tipoRefeicao = document.getElementById("tipoRefeicao").value;
 
-    // Reseta as exibições
-    document.getElementById("resultado-completo").style.display = "none";
-    document.getElementById("resultado-rapido").style.display = "none";
+    resultadoCompleto.style.display = "none";
+    resultadoRapido.style.display = "none";
 
     if (tipoRefeicao === "completo") {
-        // Gerar refeição completa (prato principal, guarnição e salada)
         const pratoAleatorio = pratosPrincipais[Math.floor(Math.random() * pratosPrincipais.length)];
         const guarnicaoAleatoria = guarnicoes[Math.floor(Math.random() * guarnicoes.length)];
         const saladaAleatoria = saladas[Math.floor(Math.random() * saladas.length)];
+        
+        pratoPrincipal.innerText = pratoAleatorio.nome;
+        guarnicao.innerText = guarnicaoAleatoria.nome;
+        salada.innerText = saladaAleatoria.nome;
+        
+        verificadoCompleto.innerHTML = pratoAleatorio.verificado ? 'Verificado' : 'Não Verificado'
+        verificadoCompleto.style.color = pratoAleatorio.verificado ? 'green' : 'red'
 
-        document.getElementById("prato-principal").innerText = pratoAleatorio.nome;
-        document.getElementById("guarnicao").innerText = guarnicaoAleatoria.nome;
-        document.getElementById("salada").innerText = saladaAleatoria.nome;
+        let etapa = ''
+        listaCompleto.innerHTML = ''
 
-        // Exibe a seção de pratos completos
-        document.getElementById("resultado-completo").style.display = "inline-block";
+        for (let i = 0; i < pratoAleatorio.receita.length; i++) {
+            const passo = pratoAleatorio.receita[i];
+            
+            etapa += `<li> ${passo} </li>`
+        }
+
+        listaCompleto.innerHTML = etapa
+
+        let topico = ''
+        itensCompleto.innerHTML = ''
+
+        for (let ingrediente in pratoAleatorio.ingredientes) {
+            const item = ingrediente
+            const quantidade = pratoAleatorio.ingredientes[ingrediente]
+
+            topico += `<li> ${item}: ${quantidade} </li>`
+          }
+
+        itensCompleto.innerHTML = topico
+        resultadoCompleto.style.display = "inline-block";
+        
     } else if (tipoRefeicao === "rapido") {
-        // Gerar refeição rápida (prato rápido)
+
         const pratoRapidoAleatorio = pratosRapidos[Math.floor(Math.random() * pratosRapidos.length)];
 
-        document.getElementById("prato-rapido").innerText = pratoRapidoAleatorio.nome;
+        pratoRapido.innerText = pratoRapidoAleatorio.nome;
 
-        // Exibe a seção de pratos rápidos
-        document.getElementById("resultado-rapido").style.display = "inline-block";
+        verificadoRapido.innerHTML = pratoRapidoAleatorio.verificado ? 'Verificado' : 'Não Verificado'
+        verificadoRapido.style.color = pratoRapidoAleatorio.verificado ? 'green' : 'red'
+
+        let etapa = ''
+        listaRapido.innerHTML = ''
+
+        for (let i = 0; i < pratoRapidoAleatorio.receita.length; i++) {
+            const passo = pratoRapidoAleatorio.receita[i];
+            
+            etapa += `<li> ${passo} </li>`
+        }
+
+        listaRapido.innerHTML = etapa
+
+        let topico = ''
+        itensRapido.innerHTML = ''
+
+        for (let ingrediente in pratoRapidoAleatorio.ingredientes) {
+            const item = ingrediente
+            const quantidade = pratoRapidoAleatorio.ingredientes[ingrediente]
+
+            topico += `<li> ${item}: ${quantidade} </li>`
+          }
+
+        itensRapido.innerHTML = topico
+        resultadoRapido.style.display = "inline-block";
     }
+}
+
+function verReceita(tipoReceita){
+    receitaCompleto.style.display = "none";
+    receitaRapido.style.display = "none";
+
+    document.getElementById("receita" + tipoReceita).style.display = 'inline-block'
+
+}
+
+function verIngredientes(tipoReceita){
+    ingredientesCompleto.style.display = "none";
+    ingredientesRapido.style.display = "none";
+
+    document.getElementById("ingredientes" + tipoReceita).style.display = 'inline-block'
+}
+
+function hideReceitas(){
+    receitaCompleto.style.display = "none";
+    receitaRapido.style.display = "none";
+}
+
+function hideResulto(){
+    resultadoCompleto.style.display = 'none'
+    resultadoRapido.style.display = 'none'
 }
 
 // Chama a função para carregar os JSONs assim que a página carregar
